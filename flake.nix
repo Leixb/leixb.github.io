75e8d7e17aa05db4272a35055a2e19e6c84db07f
LeixB <abone9999@gmail.com>
2022-06-01 12:50:55 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  outputs = {self, nixpkgs}:
+
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in
+  {
+    devShells.${system}.default = with pkgs; mkShell {
+      buildInputs = [ nodejs nodePackages.pnpm ];
+    };
+  };
+}
