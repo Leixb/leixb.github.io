8ef49a6e7cd6067f5db7193accf7f227f1dc7e4c
LeixB <abone9999@gmail.com>
2022-06-01 17:22:42 +0200

    Use newest pnpm and node

--- flake.nix
+++ flake.nix
@@ -4,10 +4,21 @@
   let
     system = "x86_64-linux";
     pkgs = import nixpkgs {inherit system;};
+
+    pnpm = pkgs.nodePackages_latest.pnpm.override {
+      preRebuild = ''
+        mkdir -p $out/bin/
+        for i in pnpm pnpx; do
+          ln -s $out/lib/node_modules/pnpm/bin/$i.cjs $out/bin/$i
+        done
+      '';
+    };
   in
   {
-    devShells.${system}.default = with pkgs; mkShell {
-      buildInputs = [ nodejs nodePackages.pnpm ];
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = [ pnpm pkgs.nodejs_latest ];
     };
+
+    packages.${system}.pnpm = pnpm;
   };
 }
